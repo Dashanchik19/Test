c6f717f6f1e2cf858160923594e27decd1355c25
Daria Tsyplinska <ananasikg15@gmail.com>
2025-05-18 16:39:29 +0300

    Додала новий контент

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  let btn = document.querySelector(".btn");
+
+  btn.addEventListener("click", () => {
+    alert("Thanks for click!");
+  });
+});
